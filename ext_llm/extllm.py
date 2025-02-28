5eb821ef5d5f7eb83a6e35ef19393c4076956b3e
giovanni <giovi.5057@gmail.com>
2025-02-28 16:25:50 +0100

    boh

--- ext_llm/extllm.py
+++ ext_llm/extllm.py
@@ -1,5 +1,5 @@
 from ext_llm import ExtLlmContext
-import ext_llm
+import ext_llm.llm as llms
 
 class ExtLlm:
 
@@ -9,10 +9,12 @@ class ExtLlm:
     def list_available_models(self):
         return self.context.get_configs()["models"]
 
-    def get_model(self, model_name: str) -> ext_llm.llm.Llm:
+    def get_model(self, model_name: str, module_name="ext_llm.llm") -> llms.Llm:
         class_name = self.context.get_configs()["models"][model_name]["class_name"]
-        module_name = "ext_llm.llm"
-        module = __import__(module_name, fromlist=[class_name])
+        try:
+            module = __import__(self.context.get_configs()["models"][model_name]["module_name"], fromlist=[class_name])
+        except KeyError:
+            module = __import__(module_name, fromlist=[class_name])
         if hasattr(module, class_name):
             return getattr(module, class_name)()
         else:
